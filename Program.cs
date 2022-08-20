851748c4aa7542cbc08e1421cb0b7af9dc3efdf4
florinrelea <florinrelea65@gmail.com>
2022-08-21 00:41:52 +0300

    Created a namespace and a class

--- Program.cs
+++ Program.cs
@@ -1,26 +1,40 @@
 ﻿// Pinger
+using Main;
 using System.Net.NetworkInformation;
 using System.Text;
 
-string dataToSend = "Ping form Florin";
-byte[] buffer = Encoding.ASCII.GetBytes(dataToSend);
-int timeout = 120;
-// Google's ip
-string serverAddress = "4.2.2.2"; 
+Pinger pinger = new Main.Pinger();
 
-Ping pingSender = new Ping();
-PingOptions options = new PingOptions();
+pinger.pingGoogle();
 
-options.DontFragment = true;
+namespace Main {
+    public class Pinger
+    {
+        public void pingGoogle()
+        {
+            string dataToSend = "Ping form Florin";
+            byte[] buffer = Encoding.ASCII.GetBytes(dataToSend);
+            int timeout = 120;
+            // Google's ip
+            string serverAddress = "4.2.2.2";
 
-PingReply reply = pingSender.Send(serverAddress, timeout, buffer, options);
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
 
-if (reply.Status == IPStatus.Success)
-{
-    Console.WriteLine("Response: {0}", reply.Status.ToString());
-    Console.WriteLine("RoundTrip: {0} ms.", reply.RoundtripTime);
-    Console.WriteLine("Time to live {0}", reply.Options.Ttl);
-} else
-{
-    Console.WriteLine("Something went wrong.");
+            options.DontFragment = true;
+
+            PingReply reply = pingSender.Send(serverAddress, timeout, buffer, options);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Response: {0}", reply.Status.ToString());
+                Console.WriteLine("RoundTrip: {0} ms.", reply.RoundtripTime);
+                Console.WriteLine("Time to live {0}", reply.Options.Ttl);
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong.");
+            }
+        }
+    }
 }
